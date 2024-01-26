9606ddb9710aa9cad7e61ee8345bc6679778e104
eunseoryu <22100466@handong.ac.kr>
2024-01-26 15:20:15 +0900

    20240126 update

---
+++ 24-1 PPS/week2/A133_유은서_20230126.py
@@ -0,0 +1,9 @@
+class Solution:
+    def capitalizeTitle(self, title: str) -> str:
+        ls = title.split(" ")
+        a = []
+        for i in ls:
+            if len(i)==1 or len(i)==2: a.append(i.lower())
+            else: a.append(i.capitalize())
+        b= ' '.join([str(elem) for elem in a])
+        return b
