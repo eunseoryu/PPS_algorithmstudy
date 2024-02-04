4f33c41b4a354dbbab68f0124a4fb59caeedea53
eunseoryu <22100466@handong.ac.kr>
2024-02-04 21:44:53 +0900

    update in 20240204

---
+++ 24-1 PPS/week3/B010_유은서_20240204.cpp
@@ -0,0 +1,37 @@
+/**
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int height_find(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+
+        int left_height = height_find(root->left);
+        int right_height = height_find(root->right);
+
+        return max(left_height, right_height) + 1;
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+
+        int left_height = height_find(root->left);
+        int right_height = height_find(root->right);
+
+        int left_diameter = diameterOfBinaryTree(root->left);
+        int right_diameter = diameterOfBinaryTree(root->right);
+
+        return max(max(left_diameter, right_diameter), left_height + right_height);
+    }
+};
